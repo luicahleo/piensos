4a951440ce0c70dcd71ef929ab85b214315eaa6a
luicahleo <luisrolandocahuanaleon@gmail.com>
2020-08-21 08:58:42 +0200

    refactorizando

--- includes/header.php
+++ includes/header.php
@@ -1,59 +1,86 @@
 <header class="header-area">
-        <div class="header-top bg-img">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-6 col-md-7 col-12 col-sm-8">
-                        <div class="header-contact">
-                            <ul>
-                                <li><i class="fa fa-phone"></i> 954 48 60 16 5</li>
-                                <li><i class="fa fa-envelope-o"></i><a href="#">intercambiolinguistico@us.es</a></li>
-                            </ul>
-                        </div>
+    <div class="header-top bg-img" >
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 col-md-7 col-12 col-sm-8">
+                    <div class="header-contact">
+                        <ul>
+                            <li><i class="fa fa-phone"></i> 626 923 178</li>
+                            <li><i class="fa fa-envelope-o"></i><a href="#">elleylui@gmail.com</a></li>
+                        </ul>
                     </div>
-                    <div class="col-lg-6 col-md-5 col-12 col-sm-4">
-                        <div class="login-register">
-                            <ul>
-                                <li><a href="login.php">Iniciar sesion</a></li>
-                                <li><a href="register.php">Registrarse</a></li>
-                            </ul>
-                        </div>
+                </div>
+                <div class="col-lg-6 col-md-5 col-12 col-sm-4">
+                    <div class="login-register">
+                        <ul>
+                            <li><a href="../login.php">Iniciar sesion</a></li>
+                            <li><a href="../register.php">Registrarse</a></li>
+                        </ul>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="header-bottom sticky-bar clearfix">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-2 col-md-6 col-4">
-                        <div class="logo">
-                            <a href="index.php">
-                                <img alt="" src="assets/img/logo/logo.png">
-                            </a>
-                        </div>
+    </div>
+    <div class="header-bottom sticky-bar clearfix stick">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-2 col-md-6 col-4">
+                    <div class="logo">
+                        <a href="../index.php">
+                            <img alt="" src="index(original)_files/logo.png" width="100" height="40" >
+                        </a>
                     </div>
-                    <div class="col-lg-10 col-md-6 col-8">
-                        <div class="menu-cart-wrap">
-                            <div class="main-menu">
-                                <nav>
-                                    <ul>
-                                        <li><a href="index.php"> INICIO </a>
+                </div>
+                <div class="col-lg-10 col-md-6 col-8">
+                    <div class="menu-cart-wrap">
+                        <div class="main-menu">
+                            <nav>
+                                <ul>
+                                    <li><a href="../index.php"> INICIO </a>
 
-                                        </li>
-                                        <li><a href="about-us.php"> Quienes somos </a></li>
+                                    </li>
+                                    <li><a href="../about-us.php"> QUIENES SOMOS  </a></li>
+                                    <li><a href="../citaprevia.php"> CITA PREVIA </a></li>
 
 
-                                        <li><a href="blog.php"> BLOG </a>
+                                    <li><a href="../contact.php"> CONTACTANOS </a></li>
+                                </ul>
+                            </nav>
+                        </div>
+                        <div class="cart-search-wrap">
 
-                                        </li>
-                                        <li><a href="contact.php"> CONTACTAR </a></li>
-                                    </ul>
-                                </nav>
+                            <div class="header-search">
+                                <button class="search-toggle">
+                                    <i class="fa fa-search"></i>
+                                </button>
+                                <div class="search-content">
+                                    <form action="#">
+                                        <input type="text" placeholder="Buscar">
+                                        <button>
+                                            <i class="fa fa-search"></i>
+                                        </button>
+                                    </form>
+                                </div>
                             </div>
-
                         </div>
                     </div>
                 </div>
+            </div>
+            <div class="mobile-menu-area">
+                <div class="mobile-menu">
+                    <nav id="mobile-menu-active" style="display: block;">
+                        <ul class="menu-overflow">
+                            <li><a href="../index.php">INICIO</a>
 
+                            </li>
+                            <li><a href="../about-us.php">QUIENES SOMOS</a></li>
+                            <li><a href="../citaprevia.php">CITA PREVIA</a></li>
+                            <li><a href="../contact.php">contactanos</a></li>
+
+                        </ul>
+                    </nav>
+                </div>
             </div>
         </div>
+    </div>
 </header>
